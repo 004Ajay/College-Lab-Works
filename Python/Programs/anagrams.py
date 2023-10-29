58b7d00007616065e8048b27af202b73c8d3a5b1
Ajay T Shaju <ajaytshaju@gmail.com>
2023-10-29 15:36:17 +0530

    deleted some files from python

--- Python/Programs/anagrams.py
+++
@@ -1,9 +0,0 @@
-# Anagrams: A meaningful word made by rearranging another word
-
-word1 = input("Enter a word: ").lower()
-word2 = input("Enter rearranged word: ").lower()
-
-if sorted(word1) == sorted(word2): # sorting & matching both words
-    print(f"{word1} & {word2} are anagrams")
-else:
-    print(f"{word1} & {word2} are not anagrams")
